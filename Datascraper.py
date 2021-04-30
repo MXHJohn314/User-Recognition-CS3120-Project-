47e4a919060944cf15c41f56a635f34b88141b92
GalacticWafer <mxhjohnson4dba@gmail.com>
2021-04-30 17:02:58 -0600

    - reduced lag for colorization; now only colorizes the last three words and spaces of text typed by the user. - added todos and testing files for Datascraper.py

--- Datascraper.py
+++ Datascraper.py
@@ -1,47 +1,38 @@
-import csv
 import pandas as pd
 
-# average key down time
-# average key roll-over time
-# most accurate keys ranking
-# lshift/rshift ratio
-# number of times pressed backspace
-# train time
-# test time
-# train/test ratio
-# mistake count
-# mistake ratio
-test = pd.read_csv('test.csv', delimiter='::', engine='python').values.tolist()
-train = pd.read_csv('train.csv', delimiter='::', engine='python').values.tolist()
+test = pd.read_csv('scape_test.csv', delimiter='::', engine='python').values.tolist()
+train = pd.read_csv('scape_train.csv', delimiter='::', engine='python').values.tolist()
 
-# testa = test[::2]
-# testb = test[1::2]
-# for a, b in zip(testa, testb):
-#     print(f'down time ="{b[0] - a[0]}"')
-# 
-# 
-# testa = test[1::2]
-# testb = test[2::2]
-# for a, b in zip(testa, testb):
-#     print(f'down time ="{b[0] - a[0]}"')
 
+# Todod average key down time
+# Todod average key roll-over time
 downs = [int(b[0]) - int(a[0]) for a, b in zip(test[::2], test[1::2])]
 rollover = [int(b[0]) - int(a[0]) for a, b in zip(test[1::2], test[2::2])]
+avg_down = sum(downs) / len(downs)
+avg_rollover = sum(rollover) / len(rollover)
+print(f'avg down time ={avg_down}\n'
+      f'avg rollover ={avg_rollover}\n')
 
-string = ''.join([_[2] for _ in test[::2]])
-print(string)
+
+# Todod number of times pressed backspace
+#  user_string = the full string that was created by the user.
+user_string = ''.join([_[2] for _ in test[::2]])
+print(f'user_string before manipulation:\n\t"{user_string}"')
 target = 'backspace'
-while string.startswith(target):
-    string = string[len(target):]
-while target in string:
-    idx = string.index(target)
-    string = string[:idx - 1] + string[idx + len(target):]
-# avg_down = sum(downs) / len(downs)
-# avg_rollover = sum(rollover) / len(rollover)
-# print(f'avg down time ={avg_down}\n'
-#       f'avg rollover ={avg_rollover}\n')
-# for _ in downs:
-#     print(_)
-# print(downs)
-# print(rollover)
-print(string)
+backspace_count = 0
+while user_string.startswith(target):
+    user_string = user_string[len(target):]
+    backspace_count += 1
+while target in user_string:
+    backspace_count += 1
+    idx = user_string.index(target)
+    user_string = user_string[:idx - 1] + user_string[idx + len(target):]
+print(f'user_string after manipulation:\n\t"{user_string}"\n')
+
+# Todo lshift/rshift ratio
+# Todo train time
+# Todo test time                                                    \n\t
+# Todo train/test ratio
+# Todo mistake count
+# Todo mistake ratio
+# Todo most accurate keys ranking
