3f3b9e03ed7227f8d0ae814bb57dc69d11c17fa5
GalacticWafer <mxhjohnson4dba@gmail.com>
2021-05-13 11:35:22 -0600

    - added 3d plotting

--- src/knn/knn_less_features.py
+++ src/knn/knn_less_features.py
@@ -15,35 +15,61 @@ names = open
 def get_cmap(n, name='hsv'):
     return plt.cm.get_cmap(name, n)
 
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 names = ['backspaces', 'holds', 'rollover', 'roll_rate', 'speed']
-combos = set()
-
+x_y_combos = set()
+x_y_z_combos = set()
 for i in range(len(names)):
     for j in range(len(names)):
         s = ''.join(sorted([names[i], names[j]]))
         if names[i] != names[j] and s.startswith(names[i]):
-            combos.add((names[i], names[j]))
+            x_y_combos.add((names[i], names[j]))
+        for k in range(len(names)):
+            s = ''.join(sorted([names[i], names[j], names[k]]))
+            if names[i] != names[j] and names[j] != names[k] and names[i] != names[k] and s.startswith(names[i] + names[j]):
+                x_y_z_combos.add((names[i], names[j], names[k]))
+print(x_y_z_combos)
 
-for i, (name1, name2) in enumerate(combos):
-    df = pd.read_csv('neighbor_train.csv')
-    kmeans = KMeans(n_clusters=33, random_state=0)
-    df['cluster'] = kmeans.fit_predict(df[[name1, name2]])
-    
-    # get centroids
-    centroids = kmeans.cluster_centers_
-    cen_x = [i[0] for i in centroids]
-    cen_y = [i[1] for i in centroids]
-    ## add to df
-    df['cen_x'] = df.cluster.map({0:cen_x[0], 1:cen_x[1], 2:cen_x[2]})
-    df['cen_y'] = df.cluster.map({0:cen_y[0], 1:cen_y[1], 2:cen_y[2]})# define and map colors
-    df['c'] = df.cluster.map({i: cmap[i] for i in range(33)}, )
-    plt.scatter(df[name1], df[name2], c=df.c, alpha = 0.6, s=10)
-    plt.xlabel(name1)
-    plt.ylabel(name2)
-    plt.show()
+def get_3d_plots():
+    for i, (name1, name2, name3) in enumerate(x_y_z_combos):
+        df = pd.read_csv('neighbor_train.csv')
+        kmeans = KMeans(n_clusters=33, random_state=0)
+        df['cluster'] = kmeans.fit_predict(df[[name1, name2, name3]])
+        fig = plt.figure(figsize=(26,6))
+        df['c'] = df.cluster.map({i: cmap[i] for i in range(33)}, )
+        ax = fig.add_subplot(131, projection='3d')
+        ax.scatter(df[name1], df[name2], df[name3], c=df.c, s=15)
+        ax.set_xlabel(name1)
+        ax.set_ylabel(name2)
+        ax.set_zlabel(name3)
+        plt.show()
+
+
+
+def get_2d_plots():
+    global i
+    for i, (name1, name2) in enumerate(x_y_combos):
+        df = pd.read_csv('neighbor_train.csv')
+        kmeans = KMeans(n_clusters=33, random_state=0)
+        df['cluster'] = kmeans.fit_predict(df[[name1, name2]])
+
+        # get centroids
+        centroids = kmeans.cluster_centers_
+        cen_x = [i[0] for i in centroids]
+        cen_y = [i[1] for i in centroids]
+        ## add to df
+        df['cen_x'] = df.cluster.map({0: cen_x[0], 1: cen_x[1], 2: cen_x[2]})
+        df['cen_y'] = df.cluster.map(
+            {0: cen_y[0], 1: cen_y[1], 2: cen_y[2]})  # define and map colors
+        df['c'] = df.cluster.map({i: cmap[i] for i in range(33)}, )
+        plt.scatter(df[name1], df[name2], c=df.c, alpha=0.6, s=10)
+        plt.xlabel(name1)
+        plt.ylabel(name2)
+        plt.show()
+
+
+get_3d_plots()
 
 
 
